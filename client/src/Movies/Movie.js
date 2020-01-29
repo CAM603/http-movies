1df465ad69be6b504765bb94849edead000f0050
CAM603 <cameronhawley@comcast.net>
2020-01-29 10:11:56 -0500

    added update component

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,10 @@
 import React from "react";
 import axios from "axios";
+
+import { Link } from 'react-router-dom';
+
 import MovieCard from "./MovieCard";
+
 export default class Movie extends React.Component {
   constructor(props) {
     super(props);
@@ -8,30 +12,30 @@ export default class Movie extends React.Component {
       movie: null
     };
   }
-
   componentDidMount() {
     this.fetchMovie(this.props.match.params.id);
   }
-
+  
   componentWillReceiveProps(newProps) {
     if (this.props.match.params.id !== newProps.match.params.id) {
       this.fetchMovie(newProps.match.params.id);
     }
   }
-
+  
   fetchMovie = id => {
     axios
-      .get(`http://localhost:5000/api/movies/${id}`)
-      .then(res => this.setState({ movie: res.data }))
-      .catch(err => console.log(err.response));
+    .get(`http://localhost:5000/api/movies/${id}`)
+    .then(res => this.setState({ movie: res.data }))
+    .catch(err => console.log(err.response));
   };
-
+  
   saveMovie = () => {
     const addToSavedList = this.props.addToSavedList;
     addToSavedList(this.state.movie);
   };
-
+  
   render() {
+    console.log(this.state.movie)
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
@@ -41,6 +45,11 @@ export default class Movie extends React.Component {
         <MovieCard movie={this.state.movie} />
         <div className="save-button" onClick={this.saveMovie}>
           Save
+        </div>
+        <div>
+          <Link to={`/update-movie/${this.state.movie.id}`}>
+            Update
+          </Link>
         </div>
       </div>
     );
---
+++ client/src/Movies/UpdateMovie.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const UpdateMovie = (props) => {
+
+    return (
+        <div>
+            <h1>Update</h1>
+            <form>
+
+            </form>
+        </div>
+    )
+}
+
+export default UpdateMovie;

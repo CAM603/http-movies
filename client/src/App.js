7ba8182f8c50f25d7f963ca73577a060c10e046d
CAM603 <cameronhawley@comcast.net>
2020-01-29 16:11:19 -0500

    moved global state to app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
+import axios from "axios";
+
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
@@ -7,8 +9,14 @@ import UpdateMovie from "./Movies/UpdateMovie";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-  console.log(savedList)
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/movies")
+      .then(res => setMovies(res.data))
+      .catch(err => console.log(err.response));
+  }, [])
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
@@ -17,7 +25,11 @@ const App = () => {
   return (
     <>
       <SavedList list={savedList} />
-      <Route exact path="/" component={MovieList} />
+      <Route 
+      exact path="/" 
+      render={props => {
+        return <MovieList {...props} movies={movies}/>
+      }}/>
       <Route
         path="/movies/:id"
         render={props => {
@@ -26,7 +38,7 @@ const App = () => {
       />
       <Route path="/update-movie/:id"
       render={props => {
-        return <UpdateMovie {...props} />
+        return <UpdateMovie {...props} movies={movies}/>
       }}
       />
     </>
